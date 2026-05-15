5e39cf7a07fffad00f6c15502d8e2dbeaf9acbab
baudelairecay <156119001+baudelairecay@users.noreply.github.com>
2026-05-14 20:48:24 -0400

    update input.c

--- input.c
+++ input.c
@@ -1,1 +1,22 @@
+#include "project.h"
 
+pi_joystick_t* joystick = NULL;
+
+void open_input(void){
+    if(joystick == NULL){
+     joystick = getJoystickDevice();
+    }
+
+}
+
+void close_input(void){
+     if(joystick != NULL){
+     	freeJoystick(joystick);
+     	joystick = NULL;
+     }
+
+}
+
+void check_input(void (*callback)(unsigned int code), int delay){
+	pollJoystick(joystick, callback, delay );
+}
